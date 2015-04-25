15436ce03e536091457f5e70744b54b49e18e9d3
sarahbkim <pnkdaegee@gmail.com>
2015-04-24 21:50:40 -0700

    add tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,8 +17,7 @@ angular.module('tictactoeApp')
     $scope.winner;
     $scope.gameOver = false;
 
-    // --- start private methods 
-    var checkRow = function(board, x, y, player) {
+    $scope.checkRow = function(board, x, y, player) {
         for(var i=0; i<board[x].length; i++) {
             if(board[x][i]!=player) {
                 return false;
@@ -27,7 +26,7 @@ angular.module('tictactoeApp')
         return true;
     }
 
-    var checkCol = function(board, x, y, player) {
+    $scope.checkCol = function(board, x, y, player) {
         for(var i=0; i<board.length;i++){
             if(board[i][y] != player) {
                 return false;
@@ -37,7 +36,7 @@ angular.module('tictactoeApp')
     }
 
     // this doesn't actually check correctly!!! 
-    var checkDiag = function(board, x, y, player) {
+    $scope.checkDiag = function(board, x, y, player) {
         if(board[0][0] != ' ') {
             if(board[0][0]== player && board[1][1] == player && board[2][2] == player) {
                 return true;
@@ -51,15 +50,15 @@ angular.module('tictactoeApp')
         }
     }
 
-    var gameOverCheck = function(x, y, player) {
+    $scope.gameOverCheck = function(x, y, player, board) {
         // check if there are no more spaces in game
         if($scope.filled==9) {
             $scope.gameOver = true;
-            $scope.winner = "None";
+            $scope.winner = 'None';
         }
-        var test1 = checkRow($scope.board, x, y, player);
-        var test2 = checkCol($scope.board, x, y, player);
-        var test3 = checkDiag($scope.board, x, y, player);
+        var test1 = $scope.checkRow(board, x, y, player);
+        var test2 = $scope.checkCol(board, x, y, player);
+        var test3 = $scope.checkDiag(board, x, y, player);
 
         if(test1 || test2 || test3) {
             $scope.gameOver = true;
@@ -69,15 +68,15 @@ angular.module('tictactoeApp')
     }
 
     // update the board
-    var updateBoard = function(x, y) {
+    $scope.updateBoard = function(x, y) {
         // update the board 
         $scope.board[x][y] =  $scope.player;
         $scope.filled++;
         var id = String(x) + String(y);
-        changeTile(id, $scope.player);
+        $scope.changeTile(id, $scope.player);
         
         // evaluate the board for winner
-        gameOverCheck(x, y, $scope.player);
+        $scope.gameOverCheck(x, y, $scope.player, $scope.board);
 
         if($scope.gameOver) {
             $scope.winner = $scope.winner || $scope.player; // this doesn't account for ties
@@ -85,7 +84,7 @@ angular.module('tictactoeApp')
         
     }
 
-    var updatePlayer = function() {
+    $scope.updatePlayer = function() {
         // new game -- start with o
         if($scope.player=='') {
             $scope.player = 'o';
@@ -99,32 +98,29 @@ angular.module('tictactoeApp')
         }
     }
 
-    var resetBoard = function() {
+    $scope.resetBoard = function() {
         $scope.board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
     }
 
-    var changeTile = function(id, player) {
-        var $currentTile = $(".tile#" + id + " div");
-        $currentTile.addClass("player_" + player);
+    $scope.changeTile = function(id, player) {
+        var $currentTile = $('.tile#' + id + ' div');
+        $currentTile.addClass('player_' + player);
     }
-
-    // --- end private methods
-
 
     $scope.play = function(x, y) {
         // return error if player chooses number not in the index
-        if(x<0 || x >$scope.board[0].length) {
-            console.log("not a valid position");
-        } else if (y < 0 || y > $scope.board.length) {
-            console.log("not a valid position");
-        } else if ($scope.board[x][y]!=' ') {
-            console.log("not a valid position - marked already!");
+        if( x< 0 || x > $scope.board[0].length-1) {
+            throw new Error('not a valid position');
+        } else if (y < 0 || y > $scope.board.length-1) {
+            throw new Error('not a valid position');
+        } else if ( $scope.board[x][y]!=' ') {
+            throw new Error('not a valid position - marked already!');
         } else {
             if(!$scope.gameOver) {
-                updatePlayer();
-                updateBoard(x, y);
+                $scope.updatePlayer();
+                $scope.updateBoard(x, y);
             } else {
-                console.log("don't update")
+                throw new Error('dont update');
             }    
         }
         
--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -7,6 +7,7 @@ describe('Controller: MainCtrl', function () {
 
   var MainCtrl,
     scope;
+  var emptyBoard = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']];
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -14,9 +15,63 @@ describe('Controller: MainCtrl', function () {
     MainCtrl = $controller('MainCtrl', {
       $scope: scope
     });
+
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should start with an empty board', function() {
+    
+    expect(scope.board).toEqual(emptyBoard);
   });
+
+  describe('MainCtrl.updatePlayer', function() {
+    
+    it('should change the player', function() {
+      expect(scope.player).toEqual('');
+      scope.updatePlayer();
+      expect(scope.player).toEqual('x');
+      scope.updatePlayer();
+      expect(scope.player).toEqual('o');
+    });
+
+  })
+
+  describe('MainCtrl.play', function() {
+
+    it('should update the board and filled variables', function() {
+      scope.updatePlayer();
+      scope.play(0, 0);
+      expect(scope.board).toEqual([['o', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]);
+      expect(scope.filled).toEqual(1);
+    });
+
+    it('should not update the board if filled', function() {
+      scope.play(0, 0);
+      scope.play(0, 1);
+      scope.play(0, 2);
+      scope.play(1, 0);
+      scope.play(1, 1);
+      scope.play(1, 2);
+      scope.play(2, 0);
+      scope.play(2, 1);
+      scope.play(2, 2);
+      
+      expect(function() {
+        scope.play(2, 2);
+      }).toThrow(new Error('not a valid position - marked already!'));
+
+    });
+
+    it('should error if you try to input to incorrect tile', function() {
+      scope.resetBoard();
+      expect(function() {
+        scope.play(0, 3);
+      }).toThrowError('not a valid position');
+
+      expect(function() {
+        scope.play(-3, 4);
+      }).toThrowError('not a valid position');
+      
+    });
+
+  })
 });
